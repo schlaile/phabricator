365dc46db1aeed144402ed7798c39c9e29429f6b
Peter Schlaile <peter@schlaile.de>
2013-10-27 12:21:56 +0100

    Additional german translations and: bugfix: umlauts are working now

--- src/infrastructure/internationalization/PhabricatorGettextTranslator.php
+++ src/infrastructure/internationalization/PhabricatorGettextTranslator.php
@@ -6,11 +6,14 @@ extends PhutilCustomTranslator {
     $root = dirname(phutil_get_library_root('phabricator'));
     $root = $root.'/resources/internationalization/mo/';
     bindtextdomain("phabricator", $root);
+    bind_textdomain_codeset("phabricator", 'UTF-8');
     setlocale(LC_MESSAGES, $lang);
   }
 
   final public function translate($text) {
-    return dgettext("phabricator", $text);
+    $rv = dgettext("phabricator", $text);
+    error_log($rv);
+    return $rv;
   }
   
   final public function plural_translate($text, $variant) {
